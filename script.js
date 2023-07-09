3d4f25811d1e76ec0de84e8b86688f911505aa14
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-07-09 22:35:31 +0530

    added files

--- script.js
+++ script.js
@@ -1,69 +1,7 @@
-/* Add some basic styling */
-body {
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-}
-
-.header {
-  background-color: #4CAF50;
-  color: white;
-  padding: 15px;
-}
-
-.header h1 {
-  margin: 0;
-}
-
-.header p {
-  margin: 0;
-}
-
-.content {
-  padding: 15px;
-}
-
-.features,
-.benefits {
-  margin-bottom: 30px;
-}
-
-.footer {
-  background-color: #4CAF50;
-  color: white;
-  padding: 15px;
-  text-align: center;
-}
-
-/* Add some basic animation */
-.fade-in {
-  animation: fadeIn ease-in-out 1s;
-}
-
-.slide-up {
-  animation: slideUp ease-in-out 1s;
-}
-
-@keyframes fadeIn {
-  from {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
-
-@keyframes slideUp {
-  from {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
+var registerButton = document.getElementById("register-button");
+registerButton.addEventListener("click", function () {
+  var registerForm = document.getElementById("register-form");
+  registerForm.submit();
+  registerForm.reset();
+  alert("You have successfully registered!");
+});
